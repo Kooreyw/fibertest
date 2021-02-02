30090e3f9fab6edd757683f06a864bc44ad232bb
kooreyw <kooreyw@gmail.com>
2021-02-02 16:34:46 +0800

    test1

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-
-void print_hello(){
-	printf("Hello world\n");
+int print_hello(int a, int b){	
+	int c=0;
+	c=a+b;
+	return c;
 }
 int main(void){
 	print_hello();
+	printf("End\n");
 	return 0;
 }
